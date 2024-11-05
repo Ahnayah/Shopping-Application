39392c42c94958cd042202dfa140b549fac6d1b6
Ahnayah <124970174+Ahnayah@users.noreply.github.com>
2024-11-05 00:23:47 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -37,14 +37,18 @@ class MainMenu:
         self.login_button.place(x=440, y=512)
         
         self.database_handler = DatabaseHandler()
-        
         self.window.mainloop()
+
     
     def login(self):
         username = self.username_entry.get()
         password = self.password_entry.get()
         if username == '' or password == '' and username == ' ' or password == ' ':
             showerror("Error", "Please enter a valid username and password")
+        elif password == '':
+            showerror("Error", "Please enter a valid password") 
+        elif username == '':
+            showerror("Error", "Please enter a valid username")
         else:
             self.database_handler.add_user(username, password)
             self.window.destroy()
@@ -76,37 +80,101 @@ class ShoppingWindow():
         
         self.greeting = Label(self.window, text=f"Welcome, {username}")
         self.greeting.place(x=140, y=30, width=1001, height=100)
-        self.greeting.config(font=("Arial",35, "bold"), bg='#939974', fg='white')
+        self.greeting.config(font=("Arial",35, "bold"), bg='#939974', fg='white')             
         
-        self.load_products("products.xlsx")
-             
+        self.database_handler = DatabaseHandler() 
+        
+        self.selected_item_entry = Text(self.window, width=20, state='disabled')
+        self.selected_item_entry.place(x=2, y=234)
+        self.selected_item_entry.config(font=("Arial", 12), height=35)
+        
+        self.load_products()
         self.window.mainloop()
     
     def send_message(self):
         ChatWindow()
         
-    def load_products(self, filename):
-        pass
-
+    def load_products(self):
+        products = self.database_handler.get_products()
+        x_position = 320
+        y_position = 260
+        for product in products:
+            product_id, name, price, image_path = product
+            product_image = PhotoImage(file=image_path)
+            
+            product_label = Label(self.window, image=product_image)
+            product_label.image = product_image
+            product_label.config(width=100, height=100)
+            product_label.place(x=x_position, y=y_position)
+            
+            product_name_label = Label(self.window, text=name, font=("Arial", 12, "bold"), bg='#939974', fg='white')
+            product_name_label.place(x=x_position, y=y_position + 105)
+            
+            product_price_label = Label(self.window, text=f"${price:.2f}", font=("Arial", 12), bg='#939974', fg='white')
+            product_price_label.place(x=x_position, y=y_position + 130)
+            
+            quanity_label = Label(self.window, text=f"Quantity: ", font=("Arial", 12), bg='#939974', fg='white')
+            quanity_label.place(x=x_position, y=y_position + 150)
+            
+            quantity_spinbox = Spinbox(self.window, from_=1, to=50, width=6)
+            quantity_spinbox.place(x=x_position, y=y_position + 170)
+            
+            select_button = Button(self.window, text="Select", bg='white', fg='black', command=lambda product_id=product_id, name=name, price=price, quantity_spinbox=quantity_spinbox: self.select_product(product_id, name, price, quantity_spinbox))
+            select_button.place(x=x_position, y=y_position + 200)
+            
+            x_position += 320
+            if x_position > 1000:
+                x_position = 320
+                y_position += 260
+                
+    def select_product(self, product_id, name, price, quantity_spinbox):
+        try:
+            quantity = int(quantity_spinbox.get())
+            if quantity > 50:
+                showerror("Error", "Please enter a valid quantity")
+                return
+            self.add_to_cart(product_id, name, price, quantity)
+        except ValueError:
+            showerror("Error", "Please enter a valid quantity")
+            
+    def add_to_cart(self, product_id, name, price, quantity):
+        user_id = self.database_handler.get_user_id(self.username)
+        self.database_handler.add_to_cart(user_id, product_id, name, price, quantity)
+        self.update_selected_item_entry()
+        
+    def update_selected_item_entry(self):
+        user_id = self.database_handler.get_user_id(self.username)
+        cart_items = self.database_handler.get_cart_items(user_id)
+        self.selected_item_entry.config(state='normal')
+        self.selected_item_entry.delete("1.0", END)
+        for item in cart_items:
+            name, price, quantity = item
+            item_details = f"Name: {name}\nPrice: ${price:.2f}\nQuantity: {quantity}\n\n"
+            self.selected_item_entry.insert(END, item_details)
+        self.selected_item_entry.config(state='disabled')
+        
 class ChatWindow:
     def __init__(self):
-        self.window = Toplevel()
+        self.window = Tk()
         self.window.geometry("600x400")
         self.window.title("Glow Getter : Message Staff")
         self.window.resizable(False, False)
+        self.window.config(bg="#939974")
         
-        self.chat_background1 = PhotoImage(file="assets/chat_background.png")
-        self.background_label2 = Label(self.window, image=self.chat_background1)
-        self.background_label2.pack()
+        self.send_button = Button(self.window, text="Send", bg='white', fg='black')
+        self.send_button.place(x=500, y=300)
         
-        self.chat_box = Text(self.window, width=70, height=15, state=DISABLED)
-        self.chat_box.pack(pady=10)
+        self.chat_box = Text(self.window)
+        self.chat_box.pack()
+        self.chat_box.config(font=("Arial", 12), width=50, height=10)
+        self.chat_box.place(x=73, y=100)
+        message = self.chat_box.get("1.0", "end-1c")
+        self.chat_box.insert(END, f'{message}')
         
-        self.chat_entry = Entry(self.window, width=70)
-        self.chat_entry.pack(side=LEFT, padx=10)
-        
-        self.send_button = Button(self.window, text="Send")
-        self.send_button.pack()
-        
+        self.sender_box = Entry(self.window)
+        self.sender_box.pack()
+        self.sender_box.config(font=("Arial", 12), width=50, state='readonly')
+        self.sender_box.place(x=73, y=70)
+
 if __name__ == "__main__":
-    MainMenu()  
+    ShoppingWindow("Test")
--- database.py
+++ database.py
@@ -20,8 +20,8 @@ class DatabaseHandler:
                 name TEXT NOT NULL,
                 price REAL NOT NULL,
                 image_path TEXT
-                )
-            ''')
+            )
+        ''')
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS orders (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,7 +33,18 @@ class DatabaseHandler:
                 FOREIGN KEY (product_id) REFERENCES products (id)
             )
         ''')
-        
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS cart (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                product_id INTEGER NOT NULL,
+                name TEXT NOT NULL,
+                price REAL NOT NULL,
+                quantity INTEGER NOT NULL,
+                FOREIGN KEY (user_id) REFERENCES users (id),
+                FOREIGN KEY (product_id) REFERENCES products (id)
+            )
+        ''')
         self.connection.commit()
 
     # Add user to database
@@ -47,6 +58,14 @@ class DatabaseHandler:
         except sqlite3.IntegrityError:
             return False
         return True
+    
+    def get_user_id(self, username):
+        self.cursor.execute('''
+            SELECT id FROM users
+            WHERE username = ?
+        ''', (username,))
+        return self.cursor.fetchone()[0]
+    
     
     # Product display from database
     def get_products(self):
@@ -62,11 +81,11 @@ class DatabaseHandler:
         ''', (name, price, image_path))
         self.connection.commit()
 
-    def add_order(self, user_id, product_id, quantity, total_price):
+    def add_to_cart(self, user_id, product_id, name, price, quantity):
         self.cursor.execute('''
-            INSERT INTO orders (user_id, product_id, quantity, total_price)
-            VALUES (?, ?, ?, ?)
-        ''', (user_id, product_id, quantity, total_price))
+            INSERT INTO cart (user_id, product_id, name, price, quantity)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (user_id, product_id, name, price, quantity))
         self.connection.commit()
     
     def close(self):
